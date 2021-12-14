c8911d4477f1e5d6622ff2a76a3c4c9c14162386
Wassim AZIRAR <wassim.azirar@gmail.com>
2021-12-14 11:55:37 +0100

    add more middlewares and create a simple route

---
+++ src/APIServer.ts
@@ -0,0 +1,82 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express, { Express } from "express";
+import flash from "express-flash";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from "hpp";
+import * as http from "http";
+import lusca from "lusca";
+import morgan from "morgan";
+import responseTime from "response-time";
+
+export default class APIServer {
+  private _app: Express;
+  get app(): Express {
+    return this._app;
+  }
+
+  private _server: http.Server;
+  get server(): http.Server {
+    return this._server;
+  }
+
+  constructor() {
+    this._app = express();
+
+    dotenv.config();
+
+    // set the port
+    this._app.set("port", process.env.PORT || 3000);
+
+    // configure middlewares
+    this.configureMiddlewares();
+  }
+
+  public configureMiddlewares(): void {
+    this._app.disable("x-powered-by");
+
+    const limiter = rateLimit({
+      windowMs: 1 * 60 * 1000, // 1 minutes
+      max: 15, // limit each IP to 15 requests per windowMs
+      message: "Too many accounts created from this IP, please try again after an 5 minutes"
+    });
+
+    this._app.use(bodyParser.json());
+    this._app.use(bodyParser.urlencoded({ extended: true }));
+    this._app.use(compression());
+    this._app.use(cookieParser());
+    this._app.use(cors());
+    this._app.use(express.json());
+    this._app.use(express.urlencoded({ extended: false }));
+    this._app.use(flash());
+    this._app.use(helmet());
+    this._app.use(hpp());
+    this._app.use(limiter);
+    this._app.use(lusca.xframe("SAMEORIGIN"));
+    this._app.use(lusca.xssProtection(true));
+    this._app.use(morgan("dev"));
+    this._app.use(responseTime());
+
+    this._app.use((request, response, next) => {
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.setHeader("Access-Control-Allow-Credentials", "true");
+      response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      response.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+      );
+
+      next();
+    });
+  }
+
+  public start(): void {
+    this._server = this._app.listen(this._app.get("port"), () => {
+      console.log(`API server is running at http://localhost:${this._app.get("port")}`);
+    });
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,38 @@
-  function toto() {
-			console.log('Hello')
-				console.log('world')
-		}
+import { Request, Response } from "express";
+import APIServer from "./APIServer";
+
+const server = new APIServer();
+
+const route = (method: string, path: string) => {
+  return (target: object, propertyKey: string, descriptor: PropertyDescriptor) => {
+    server.app[method](path, (request: Request, response: Response) => {
+      response.status(200).json(descriptor.value(request, response));
+    });
+  };
+};
+
+const authorize = () => {
+  return (target: object, propertyKey: string, descriptor: PropertyDescriptor) => {
+    console.log(process.env.API_KEY);
+    const original = descriptor.value;
+    descriptor.value = (...args: unknown[]) => {
+      const req = args[0] as Request;
+      const res = args[1] as Response;
+      const headers = req.headers;
+      if (headers.hasOwnProperty("apikey") && headers.apikey == process.env.API_KEY) {
+        return original.apply(this, args);
+      }
+      res.status(403).json({ error: "Not Authorized" });
+    };
+  };
+};
+
+export default class APIRoutes {
+  @route("get", "/")
+  @authorize()
+  public indexRoute(request: Request, response: Response) {
+    return "Hello world";
+  }
+}
+
+server.start();
